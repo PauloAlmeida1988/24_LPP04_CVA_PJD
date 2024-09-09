7126d00f11408cbfc9d419bcafbf292e7ee7e83d
PauloAlmeida1988 <pauloricardoalmeida79@gmail.com>
2024-09-09 10:46:47 +0100

    Criação do projeto
    Criação do projeto!
    
    Co-Authored-By: PauloAlmeida1988 <180940741+pauloalmeida1988@users.noreply.github.com>

---
+++ Média Notas.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+nota_teste1 = float(input("Por favor, insira a nota do teste 1: "))
+nota_teste2 = float(input("Por favor, insira a nota do teste 2: "))
+media = (nota_teste1 + nota_teste2) / 2
+print(f"A média das notas é: {media:.2f}")
