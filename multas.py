a4cc9ea919e1e4f53ac23b29312a29159438834b
PauloAlmeida1988 <pauloricardoalmeida79@gmail.com>
2024-09-10 12:19:33 +0100

    Add files via upload

---
+++ multas.py
@@ -0,0 +1,8 @@
+velocidade = float(input("Qual é a velocidade do carro em km/h? "))
+if velocidade > 80:
+    excesso = velocidade - 80
+    multa = excesso * 5.00
+    print(f"Você foi multado por excesso de velocidade.")
+    print(f"Valor da multa: €{multa:.2f}")
+else:
+    print("Você está dentro do limite de velocidade.")
